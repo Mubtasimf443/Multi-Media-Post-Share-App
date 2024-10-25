60132b98fea00db2b89c2c670a656ac8779c5765
Mubtasim <mubtasimf443@gmail.com>
2024-10-25 17:22:28 +0600

    app, client (next.js) ,server added

---
+++ app/App.js
@@ -0,0 +1,25 @@
+/*
+بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ  ﷺ  
+InshaAllah, By his marcy I will Gain Success 
+*/
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ server/index.js
@@ -0,0 +1,19 @@
+/*
+بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ  ﷺ  
+InshaAllah, By his marcy I will Gain Success 
+*/
+
+import express from 'express'
+import {log} from 'console'
+
+
+
+
+const app = express();
+
+
+
+
+
+
+app.listen(4000, e => log('Allhamdulillah') )
